41a7dcab08000f0703ef5f3b95661817f5db24ea
Esteban Mares <estebanmares17@gmail.com>
2022-07-03 14:57:53 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
