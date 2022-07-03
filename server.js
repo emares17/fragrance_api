bbda8c94a40efdb546431a6873d90a17c5f8dfde
Esteban Mares <estebanmares17@gmail.com>
2022-07-03 15:29:44 -0700

    added remaining routes

--- server.js
+++ server.js
@@ -5,9 +5,25 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config/config.env' });
 
-app.get('/', (req, res) => {
-    res.send('hello from express');
-})
+app.get('/api/v1/', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show all '})
+});
+
+app.get('/api/v1//:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Get ${req.params.id}`})
+});
+
+app.post('/api/v1/', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Create new '})
+});
+
+app.put('/api/v1//:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Update ${req.params.id}`})
+});
+
+app.delete('/api/v1//:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete ${req.params.id}`})
+});
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
