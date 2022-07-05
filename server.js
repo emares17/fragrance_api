e128507104bfcd4ef191bcda0bea50ae9697f3c8
Esteban Mares <estebanmares17@gmail.com>
2022-07-04 17:16:43 -0700

    small changes/added morgan

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const dotenv = require('dotenv');
+const morgan = require('morgan')
+
+dotenv.config({ path: './config/config.env' });
 
 // Routes
 const perfumeries = require('./routes/perfumeries');
 
-dotenv.config({ path: './config/config.env' });
+//Logging middleware in development 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
 
 // Mount Routes
 app.use('/api/v1/perfumeries', perfumeries);
