4c59fb31c6271502143a3790c1b3543b773edd25
Esteban Mares <estebanmares17@gmail.com>
2022-07-06 19:53:40 -0700

    added connection to DB

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const dotenv = require('dotenv');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const connectDB = require('./config/db')
 
+// Load env variables
 dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+connectDB();
 
 // Routes
 const perfumeries = require('./routes/perfumeries');
@@ -17,5 +22,12 @@ if (process.env.NODE_ENV === 'development') {
 // Mount Routes
 app.use('/api/v1/perfumeries', perfumeries);
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server and exit
+    server.close(() => process.exit(1))
+});
 
